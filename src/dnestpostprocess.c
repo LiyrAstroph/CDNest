0c760700804d3ab48eb08cc40d0ce81b83d0b4cd
Yan-Rong Li <liyanrong@mail.ihep.ac.cn>
2022-08-10 21:05:59 +0800

    back to previous format of posterior sample file.

--- src/dnestpostprocess.c
+++ src/dnestpostprocess.c
@@ -403,7 +403,7 @@ void postprocess(double temperature)
     fprintf(stderr, "# Error: Cannot open file %s.\n", options.posterior_sample_file);
     exit(0);
   }
-  fprintf(fp, "# %d %f\n", num_ps, temperature);
+  fprintf(fp, "# %d\n", num_ps);
 
   for(i=0; i<num_ps; i++)
   {
